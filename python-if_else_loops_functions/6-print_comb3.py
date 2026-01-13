7287358ffce41649167919cfb36646468fa627c1
ahotb <ah.r9625@gmail.com>
2026-01-13 18:50:07 +0300

    fixed pycodestyle

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print(', '.join("{}{}".format(num, num2) for num in range(10) for num2 in range(num + 1, 10)))
+print(', '.join("{}{}".format(num, num2)
+				for num in range(10) for num2 in range(num + 1, 10)))
