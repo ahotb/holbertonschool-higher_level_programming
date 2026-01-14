686ef1822b2a83ae57489cecd46b963369031ac5
ahotb <ah.r9625@gmail.com>
2026-01-14 13:03:40 +0300

    fixed code

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-for code in range(ord("z"), ord("a") - 1, -1):
-    cher = chr(code)
-    position = ord("z") - code
-    if position % 2 == 0:
-        print(cher.lower(), end="")
-    else:
-        print(cher.upper(), end="")
+for ch in range(ord("z"), ord("a") - 1, -1):
+    print(chr(ch).lower() if (ord('z') - ch) % 2 == 0 else chr(ch).upper(),end="")
