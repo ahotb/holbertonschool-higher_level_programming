d696ee6b2f52f0a1b610080b4f267b5a13530a0f
ahotb <ah.r9625@gmail.com>
2026-01-14 11:36:37 +0300

    fixed upper char

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for x in range(1, 101):
         if x % 15 == 0:
-            print("fizzbuzz", end=" ")
+            print("FizzBuzz", end=" ")
         elif x % 5 == 0:
-            print("buzz", end=" ")
+            print("Buzz", end=" ")
         elif x % 3 == 0:
-            print("fizz", end=" ")
+            print("Fizz", end=" ")
         else:
             print(x, end=" ")
