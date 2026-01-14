c899defb542984a4148201127a7358bfddb550c1
ahotb <ah.r9625@gmail.com>
2026-01-14 13:08:02 +0300

    fix

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for ch in range(ord("z"), ord("a") - 1, -1):
-    print(chr(ch).lower() if (ord('z') - ch) % 2 == 0 else chr(ch).upper(),end="")
+    print(chr(ch).lower() if (ord("z") - ch) % 2 == 0 else chr(ch).upper(), end="")
