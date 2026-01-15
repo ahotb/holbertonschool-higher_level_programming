f6399aa69627e110ce320cfd22af04a1dfb0efe1
ahotb <ah.r9625@gmail.com>
2026-01-15 23:21:36 +0300

    fix spec

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,11 +4,11 @@ from sys import argv
 if __name__ == "__main__":
     leng = len(argv) - 1
     if leng > 1:
-     print("{} arguments:".format(leng))
+        print("{} arguments:".format(leng))
     elif leng == 0:
-     print("{} arguments.".format(leng))
+        print("{} arguments.".format(leng))
     elif leng == 1:
-     print("1 argument:")
+        print("1 argument:")
 
     for i in range(1, leng + 1):
 	    print("{}: {}".format(i, argv[i]))
