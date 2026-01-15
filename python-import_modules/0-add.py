7e7da1fdc6c8f1f051733cd03bb96152bceb86d8
ahotb <ah.r9625@gmail.com>
2026-01-15 19:34:21 +0300

    addig sum

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    sum1 = a + b
+    print(f"{a} + {b} = {sum1}")
