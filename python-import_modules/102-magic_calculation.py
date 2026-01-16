c1d9730c59c8d3abde4b35bbb0165fce349a5530
ahotb <ah.r9625@gmail.com>
2026-01-16 15:03:29 +0300

    fix

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    if a < b:
-        c = a + b
-        for i in range(4, 6):
-            c = c + i
-        return c
-    else:
         return a - b
