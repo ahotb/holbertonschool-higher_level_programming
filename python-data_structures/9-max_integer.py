96ef6c99d55c5458c09cc7a6e02f256ff8e4ed48
ahotb <ah.r9625@gmail.com>
2026-01-18 18:35:15 +0300

    refactor max_integer function to manually find the maximum value in a list

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,4 +2,9 @@
 def max_integer(my_list=[]):
     if my_list == 0:
         return None
-    return max(my_list)
+
+    val_max = my_list[0]
+    for i in my_list[1:]:
+        if i > val_max:
+            val_max = i
+    return val_max
