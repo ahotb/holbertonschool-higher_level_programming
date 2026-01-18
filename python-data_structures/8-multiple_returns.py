5a44e52bfd20011b405788f4f4e99fb58999d9bc
ahotb <ah.r9625@gmail.com>
2026-01-18 18:09:14 +0300

    refactor multiple_returns to improve readability and handle empty strings

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return len(sentence), sentence[0]
+    leng = len(sentence)
+    first = sentence[0] if leng > 0 else None
+    return leng, first
