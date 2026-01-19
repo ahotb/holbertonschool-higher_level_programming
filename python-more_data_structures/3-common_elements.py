f8dddf5327d64a9b1a2ba00cafa55377a7ae031c
ahotb <ah.r9625@gmail.com>
2026-01-19 13:36:02 +0300

    refactor common_elements function to use set intersection and add test cases

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    add_set = set()
-    for x in set_1:
-        if x in set_2:
-            add_set.add(x)
-    return add_set
+    return set_1 & set_2
