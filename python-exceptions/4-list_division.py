df2f053db99a4bed181ffccb2e0277430574f067
ahotb <ah.r9625@gmail.com>
2026-01-20 19:16:58 +0300

    refactor list_division function to append results in finally block and initialize result variable

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,17 +2,18 @@
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
+        resul = 0
         try:
             resul = my_list_1[i] / my_list_2[i]
-            new_list.append(resul)
 
         except TypeError:
             print("wrong type")
-            new_list.append(0)
+
         except ZeroDivisionError:
             print("division by 0")
-            new_list.append(0)
+
         except IndexError:
             print("out of range")
-            new_list.append(0)
+        finally:
+            new_list.append(resul)
     return new_list
