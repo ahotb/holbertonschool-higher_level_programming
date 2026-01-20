a2cc9778dd210be16e117d5e287bef5adb3cd6f8
ahotb <ah.r9625@gmail.com>
2026-01-20 17:23:34 +0300

    remove IndexError handling from safe_print_list_integers function

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
             cuntr += 1
         except (ValueError, TypeError):
             continue
-        except IndexError:
-            break
+
     print()
     return cuntr
