d2ff3710bcf90fb21c4d36c928266fb710ac8c85
ahotb <ah.r9625@gmail.com>
2026-01-26 11:51:05 +0300

    fix: correct instance variable checks in Rectangle constructor

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,12 +10,12 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        if not isinstance(self.__width, int):
+        if not isinstance(width, int):
             raise TypeError("width must be an integer")
         if self.__width < 0:
             raise ValueError("width must be >= 0")
         self.__width = width
-        if not isinstance(self.__height, int):
+        if not isinstance(height, int):
             raise TypeError("height must be an integer")
         if self.__height < 0:
             raise ValueError("height must be >= 0")
