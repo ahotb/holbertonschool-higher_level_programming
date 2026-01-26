a93b8f5555da4f1a102147f1309657eee4869ca1
ahotb <ah.r9625@gmail.com>
2026-01-26 18:10:05 +0300

    feat: add Rectangle class with validation, area, perimeter, and string representation methods

---
+++ python-more_classes/7-rectangle.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python3
+"""
+Docstring for python-more_classes.0-rectangle
+"""
+
+
+class Rectangle:
+    """
+    Docstring for Rectangle
+    """
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """
+                Docstring for width
+
+                :param self: Description
+                """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+                Docstring for width
+
+                :param self: Description
+                :param value: Description
+                """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+                Docstring for height
+
+                :param self: Description
+                """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+                Docstring for width
+
+                :param self: Description
+                :param value: Description
+                """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            printt = str(self.print_symbol) * self.width
+        return '\n'.join([printt] * self.height)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.__width}, {self.__height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
