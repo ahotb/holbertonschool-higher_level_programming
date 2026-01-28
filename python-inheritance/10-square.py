4124d2903058c93cca0c81310e9388ed3891fe5c
ahotb <ah.r9625@gmail.com>
2026-01-28 16:16:08 +0300

    refactor Square class by removing redundant size attribute and methods

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -13,10 +13,4 @@ class Square (Rectangle):
 
     def __init__(self, size):
         self.integer_validator("size", size)
-        self.__size = size
-
-    def area(self):
-        return self.__size * self.__size
-
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__size, self.__size)
+        super().__init__(size, size)
