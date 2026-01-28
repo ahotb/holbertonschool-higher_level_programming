a79e67df461c2f496d1d3c3628e0542150a6d297
ahotb <ah.r9625@gmail.com>
2026-01-28 13:42:34 +0300

    feat: implement BaseGeometry class with area method and integer validation

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Docstring for python-inheritance.7-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """
+        Docstring for BaseGeometry
+        """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
