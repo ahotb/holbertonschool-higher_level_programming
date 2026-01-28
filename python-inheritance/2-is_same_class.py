2a7db71a6cc7c52cafb4fa264a73cb8fb67b62c2
ahotb <ah.r9625@gmail.com>
2026-01-28 12:18:00 +0300

    feat: add is_same_class function and main script for testing

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Docstring for python-inheritance.2-is_same_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+        Docstring for is_same_class
+
+        :param obj: Description
+        :param a_class: Description
+        """
+    if type(obj) == a_class:
+        return True
+    return False
