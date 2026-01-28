5abbb66ea06fbb6fc2ff52b9470db2b4e04b368d
ahotb <ah.r9625@gmail.com>
2026-01-28 13:02:07 +0300

    feat: add is_kind_of_class function to check class inheritance

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Docstring for python-inheritance.3-is_kind_class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+        Docstring for is_kind_class
+
+        :param obj: Description
+        :param a_class: Description
+        """
+    if isinstance(obj, a_class):
+        return True
