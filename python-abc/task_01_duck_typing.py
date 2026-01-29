ce1ce17af9d365a4f4017fe379e29d5451ef3b18
ahotb <ah.r9625@gmail.com>
2026-01-29 16:52:28 +0300

    fix

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -53,12 +53,12 @@ class Rectangle(Shape):
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
-        self.__width = width
+        self.width = width
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
-        self.__height = height
+        self.height = height
 
     def area(self):
         """
