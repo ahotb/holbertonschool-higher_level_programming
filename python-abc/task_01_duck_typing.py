f8fa32bd5ff8f563c2555516c0b6a492d1d33390
ahotb <ah.r9625@gmail.com>
2026-01-29 17:01:33 +0300

    remove unnecessary docstrings and type checks in shape classes

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -7,20 +7,10 @@ class Shape(ABC):
 
     @abstractmethod
     def area(self):
-        """
-                Docstring for area
-
-                :param self: Description
-                """
         pass
 
     @abstractmethod
     def perimeter(self):
-        """
-                Docstring for perimeter
-
-                :param self: Description
-                """
         pass
 
 
@@ -30,50 +20,22 @@ class Circle(Shape):
         self.radius = radius
 
     def area(self):
-        """
-                Docstring for area
-
-                :param self: Description
-                """
         return math.pi * (self.radius ** 2)
 
     def perimeter(self):
-        """
-                Docstring for perimeter
-
-                :param self: Description
-                """
         return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
 
     def __init__(self, width, height):
-        if not isinstance(width, int, float):
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
         self.width = width
-        if not isinstance(height, int, float):
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
         self.height = height
 
     def area(self):
-        """
-                Docstring for area
-
-                :param self: Description
-                """
         return self.width * self.height
 
     def perimeter(self):
-        """
-                Docstring for perimeter
-
-                :param self: Description
-                """
         if self.width == 0 or self.height == 0:
             return 0
         else:
