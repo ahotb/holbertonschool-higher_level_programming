d0346743e07d9c33161eb1b5d95a07b8f5ff0658
ahotb <ah.r9625@gmail.com>
2026-01-29 16:52:04 +0300

    fix code

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -49,8 +49,16 @@ class Circle(Shape):
 class Rectangle(Shape):
 
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
 
     def area(self):
         """
