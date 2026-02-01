1307c1f0f6f9194eac1f57493ac3f5791b33e09c
ahotb <ah.r9625@gmail.com>
2026-02-01 18:50:18 +0300

    fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -10,6 +10,6 @@ def read_file(filename=""):
 
         :param filename: Description
         """
-    with open(filename, 'r', encoding='utf-8') as file:
-        content = file.read()
-        print(content)
+    if filename:
+        with open(filename, 'r', encoding='utf-8') as file:
+            print(file.read())
