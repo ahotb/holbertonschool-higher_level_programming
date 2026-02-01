faf8513adb70ea705721afab59decb0aa3d7e275
ahotb <ah.r9625@gmail.com>
2026-02-01 18:41:52 +0300

    add documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+Docstring for python-input_output.0-read_file
+"""
+
 
 def read_file(filename=""):
+    """
+        Docstring for read_file
+
+        :param filename: Description
+        """
     with open('filename', 'r', encoding='utf-8') as file:
         content = file.read()
         print(content)
