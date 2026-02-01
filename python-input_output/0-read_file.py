db528141d5d8e910871f1d1dd945a07965c5e3c4
ahotb <ah.r9625@gmail.com>
2026-02-01 18:52:38 +0300

    fix style

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,9 +7,9 @@ Docstring for python-input_output.0-read_file
 def read_file(filename=""):
     """
         Docstring for read_file
-
+        Read a UTF-8 text file and print its content to stdout
         :param filename: Description
         """
     if filename:
         with open(filename, 'r', encoding='utf-8') as file:
-            print(file.read())
+            print(file.read(), end="")
