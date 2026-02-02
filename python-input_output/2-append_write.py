8401b328a15b9dd2c40eb32ba75f51adf5958fe2
ahotb <ah.r9625@gmail.com>
2026-02-02 11:52:53 +0300

    add append_write function and initial text file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Docstring for python-input_output.2-append_write
+"""
+
+
+def append_write(filename="", text=""):
+    """
+        Docstring for append_write
+        write a text
+        :param filename: Description
+        :param text: Description
+        """
+    with open(filename, 'w', encoding='utf-8') as file:
+        return file.write(text)
