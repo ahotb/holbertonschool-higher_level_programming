e8709509a004cd7968023d2486a40b2df5483dff
ahotb <ah.r9625@gmail.com>
2026-02-02 12:41:25 +0300

    add dec json

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -8,7 +8,7 @@ Docstring for python-input_output.3-to_json_string
 def to_json_string(my_obj):
     """
         Docstring for to_json_string
-
+        json used
         :param my_obj: Description
         """
     return json.dumps(my_obj)
