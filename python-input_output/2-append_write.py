780ed11e262d2b57ea99075caf1244b77ca5bf2d
ahotb <ah.r9625@gmail.com>
2026-02-02 11:54:22 +0300

    fix append_write function to use append mode instead of write mode

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -11,5 +11,5 @@ def append_write(filename="", text=""):
         :param filename: Description
         :param text: Description
         """
-    with open(filename, 'w', encoding='utf-8') as file:
+    with open(filename, 'a', encoding='utf-8') as file:
         return file.write(text)
