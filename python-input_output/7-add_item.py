88ca47407f5d358c0c2d7a4eea4529097b62691f
ahotb <ah.r9625@gmail.com>
2026-02-02 15:22:35 +0300

    adding save_to_json_file and load_from_json_file functions

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
+
+
+try:
+    existing_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    existing_list = []
+arg = sys.argv[1:]
+existing_list.extend(arg)
+save_to_json_file(existing_list, "add_item.json")
