05f39e6408ab065bdae17cdf44f4f8ee76ac5ae7
ahotb <ah.r9625@gmail.com>
2026-02-04 20:22:35 +0300

    add append_after function to insert new content after a specified string in a file

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Docstring for python-input_output.100-append_after
+"""
+import json
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+        Docstring for append_after
+
+        :param filename: Description
+        :param search_string: Description
+        :param new_string: Description
+        """
+
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    new_content = []
+    for line in lines:
+        new_content.append(line)
+        if search_string in line:
+            new_content.append(new_string)
+
+    with open(filename, 'w') as file:
+        file.writelines(new_content)
