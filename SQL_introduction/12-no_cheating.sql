ac9e89b5215539ad78f485b89b9e79bd43d478ff
ahotb <ah.r9625@gmail.com>
2026-02-24 21:39:46 +0300

    fix

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,6 +1,4 @@
 -- In this task, you are asked to update the score of a specific person in the second_table.
-SELECT score, name
-FROM second_table
 UPDATE second_table
 SET score = 10
 WHERE name = 'Bob';
