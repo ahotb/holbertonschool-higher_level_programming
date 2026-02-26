f60f3b44a619d472770aed4f1476da46441880e2
ahotb <ah.r9625@gmail.com>
2026-02-26 22:56:56 +0300

    fixed access user_2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Create a user with read-only access to all databases
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON *.db TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
