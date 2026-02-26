2b6d15f47c13c704be6833d7e0b97603a1b80e82
ahotb <ah.r9625@gmail.com>
2026-02-26 23:26:48 +0300

    fixed

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,5 @@
 --	Create a table named id_not_null with the following columns:
 CREATE TABLE IF NOT EXISTS id_not_null(
-	id INT AUTO_INCREMENT PRIMARY KEY
+	id INT DEFAULT 1 NOT NULL,
+	name varchar(256) NOT NULL
 );
