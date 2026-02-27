cc2f10d29599fda9cb095a796538d3fd1ada4e02
ahotb <ah.r9625@gmail.com>
2026-02-28 01:10:07 +0300

    fix: correct ALTER TABLE statement for UTF8 conversion in SQL script

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
--- 100-move_to_utf8.sql
+-- Convert database, table, and field to UTF8
 USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
