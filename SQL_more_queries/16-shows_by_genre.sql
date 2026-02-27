216e7962b854cc87811e0d469769d4177a274ab0
ahotb <ah.r9625@gmail.com>
2026-02-27 21:28:50 +0300

    fix: correct comment formatting in SQL query for TV shows and genres

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,4 +1,4 @@
---- Write a query to find the titles of all TV shows and their genres, ordered alphabetically by show title and genre name.
+-- Write a query to find the titles of all TV shows and their genres, ordered alphabetically by show title and genre name.
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
