9c0cc9c2f69fd126aec5db32a7a8341ed6ce31aa
ahotb <ah.r9625@gmail.com>
2026-02-27 22:40:58 +0300

    adding BINARY

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+    query = "SELECT * FROM states WHERE name = BINARY '{}' ORDER BY id ASC".format(
         state_name)
     cur.execute(query)
 
