0b86e28cbd9ee154bd17f1710a006f5f4404d916
ahotb <ah.r9625@gmail.com>
2026-02-28 01:07:35 +0300

    fix: update comment header in UTF8 conversion script for clarity

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
--- Convert database, table, and field to UTF8
+-- 100-move_to_utf8.sql
 USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
