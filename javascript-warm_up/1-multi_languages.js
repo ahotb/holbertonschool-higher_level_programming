d6af883d6ebfb0ca5cc7d53c9fd432d47f7b99b6
ahotb <ah.r9625@gmail.com>
2026-03-13 21:26:50 +0300

    fix: update string quotes in multi_languages.js for consistency

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const mylist = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const mylist = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < mylist.length; i++) {
   console.log(mylist[i]);
 }
