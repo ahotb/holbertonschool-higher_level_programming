2ccc95355eb25b8af992c4aee249d4b162830126
ahotb <ah.r9625@gmail.com>
2026-03-13 22:38:14 +0300

    fixed code

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,4 +2,10 @@
 function add(a, b) {
   return a + b;
 }
-console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+if (isNaN(num1) || isNaN(num2)) {
+  console.log('NaN');
+} else {
+  console.log(add(num1, num2));
+}
