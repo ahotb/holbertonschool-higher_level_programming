c3f429696395dc6eda525e90034a7a205a227af6
ahotb <ah.r9625@gmail.com>
2026-03-29 16:01:00 +0300

    feat: add HTML and JavaScript files for header color change on click

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,5 +1,5 @@
 const red_header = document.getElementById("red_header");
 const myheader = document.querySelector("header");
 red_header.addEventListener('click', () => {
-	myheader.style.color = "#FF0000";
+	myheader.classList.add('red');
 });
